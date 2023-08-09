05b6fab6f40050c7e4860d9333126c32f704d8f8
pengtao.geng <pengtao.geng>
2023-08-09 14:28:10 +0800

    修改测试

--- lang/apscheduler_test.py
+++ lang/apscheduler_test.py
@@ -9,8 +9,10 @@ def print_num():
     time.sleep(5)
 
 
+# BlockingScheduler：适用于调度程序是进程中唯一运行的进程，调用 start函数会阻塞当前线程，不能立即返回。
 scheduler = BlockingScheduler()
 scheduler.add_job(print_num, 'interval', seconds=1)
 scheduler.start()
 
+print("不会走到这里")
 time.sleep(10000)
