b2d415ab1c0268e24e5ecadb94d942dc0b1e7ecf
pengtao.geng <pengtao.geng>
2023-08-10 17:38:53 +0800

    修改

--- lang/apscheduler_test2.py
+++ lang/apscheduler_test2.py
@@ -6,12 +6,11 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 def print_num():
     print("hello")
-    time.sleep(5)
 
 
 # BackgroundScheduler：适用于调度程序在应用程序的后台运行，调用 start后主线程不会阻塞。
 scheduler = BackgroundScheduler()
-scheduler.add_job(print_num, 'interval', seconds=1)
+scheduler.add_job(print_num, 'interval', seconds=5)
 scheduler.start()
 
 print("会走到这里")
